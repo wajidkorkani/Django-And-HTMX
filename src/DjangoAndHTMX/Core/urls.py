d46edaed3feade8aab1bc3d4025864a475095f90
Wajid Korkani <errormrcoder@gmail.com>
2024-07-09 17:07:38 +0500

    An api has been created for test purpose and htmx is working fine.

--- src/DjangoAndHTMX/Core/urls.py
+++ src/DjangoAndHTMX/Core/urls.py
@@ -3,5 +3,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import *
 urlpatterns = [
-    path('', Home, name='home')
+    path('', Home, name='home'),
+    path('test', Test, name='test')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
